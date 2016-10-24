ebeafc7e83bf271ca392d10e02fe70c0bb57ca0b
Marat Nasibyan ashoti <marat_17.5@live.com>
2016-10-24 23:45:23 +0400

    a

--- Store/ClassLibrary1/Class1.cs
+++ Store/ClassLibrary1/Class1.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace DAL
+ 
+namespace ClassLibrary1
 {
     public class Class1
     {
+
     }
 }
